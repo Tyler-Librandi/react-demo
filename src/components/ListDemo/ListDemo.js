f4d31976a5dc7de3e94e5092b8766a90d242e029
Tyler-Librandi <TylerLibrandi@gmail.com>
2022-01-14 09:09:49 -0500

    updated theme colors and style, list, and toggle page functionality

--- src/components/ListDemo/ListDemo.js
+++ src/components/ListDemo/ListDemo.js
@@ -1,18 +1,61 @@
 import styles from "./ListDemo.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { FaWindowClose } from 'react-icons/fa';
 
 function ListDemoComponent() {
-  const [display, setDisplay] = useState('');
+  const input = useRef();
+  const [list, setList] = useState([]);
+  let id = 0;
+  let addItem = (e) => {
+    e.preventDefault();
+    if (input.current.value != "") {
+      setList([...list, input.current.value]);
+      id++;
+      input.current.value = null;
+    } else {
+      return null;
+    }
+  };
+
+  function removeThisItem(id) {
+    list.splice(id, 1);
+    setList([...list]);
+  }
 
   return (
     <div className={styles.listDemo}>
-      <h1>List Manipulation</h1>
-      <p>I've also learned how to create dynamic lists</p>
-      <p>Enter something below to add it to the list</p>
-      <button>Add</button>
-      <input type="text" onChange={(e) => {setDisplay(e.target.value)}} />
-      <div className={styles.list}>
-        <p>{display}</p>
+      <div className="row">
+        <div className="col-6">
+          <h1 className={styles.title}>List Building</h1>
+          <p className={styles.subtitle}>Enter something below to add it to the list</p>
+          <form onSubmit={addItem}>
+            <input
+              className={styles.listInput}
+              type="text"
+              ref={input}
+              placeholder="Enter text here"
+            />
+            <button className={styles.btn} type="submit">
+              Add
+            </button>
+          </form>
+        </div>
+        <div className="col-6">
+          <div className={styles.list}>
+            <ul>
+              <h2 className={styles.listTitle}>A Few of My Favorite Things</h2>
+              {list.map((item, id) => (
+                <li className={styles.output} key={id}>
+                  {item}
+                  <button className={styles.deleteBtn} key={id} onClick={() => removeThisItem(id)}>
+                  <FaWindowClose />
+                  </button>
+                  <hr />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/StylesDemo/StylesDemo.js
@@ -0,0 +1,207 @@
+import { useState } from "react/cjs/react.development";
+import styles from "./StylesDemo.module.css";
+
+function StylesDemoComponent() {
+  const [colorStyle, setColorStyle] = useState({});
+  const [bgStyle, setBgStyle] = useState({});
+  const [sizeStyle, setSizeStyle] = useState({});
+  const [borderSizeStyle, setBorderSizeStyle] = useState({});
+  const [borderColorStyle, setBorderColorStyle] = useState({});
+  const [borderRadiusStyle, setBorderRadiusStyle] = useState({});
+
+  const elementInit = (
+    <p
+      style={{
+        ...colorStyle,
+        ...bgStyle,
+        ...sizeStyle,
+        ...borderSizeStyle,
+        ...borderColorStyle,
+        ...borderRadiusStyle,
+      }}
+      className={styles.element}
+    >
+      Click any of the buttons below to change the styling of this sentence!
+    </p>
+  );
+  function changeColor(newStyle) {
+    setColorStyle({ color: newStyle });
+  }
+  function changeBGColor(newStyle) {
+    setBgStyle({ backgroundColor: newStyle });
+  }
+  function changeSize(newStyle) {
+    setSizeStyle({ fontSize: newStyle });
+  }
+  function changeBorderSize(newStyle) {
+    setBorderSizeStyle({ borderWidth: newStyle });
+  }
+  function changeBorderColor(newStyle) {
+    setBorderColorStyle({ borderColor: newStyle });
+  }
+  function changeBorderRadius(newStyle) {
+    setBorderRadiusStyle({ borderRadius: newStyle });
+  }
+  return (
+    <div className={styles.stylesDemo}>
+      <h1 className={styles.title}>Style Manipulation</h1>
+      <div className={styles.elementBox}>{elementInit}</div>
+
+      <div className={[styles.optRow, "row"].join(" ")}>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Font Size</h4>
+            <button className={styles.btn} onClick={() => changeSize("18px")}>
+              18px
+            </button>
+            <button className={styles.btn} onClick={() => changeSize("24px")}>
+              24px
+            </button>
+            <button className={styles.btn} onClick={() => changeSize("32px")}>
+              32px
+            </button>
+            <button className={styles.btn} onClick={() => changeSize("20px")}>
+              Reset
+            </button>
+          </div>
+        </div>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Font Color</h4>
+            <button
+              className={styles.btn}
+              onClick={() => changeColor("#8A00FF")}
+            >
+              Violet
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeColor("#0EF1D9")}
+            >
+              Cyan
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeColor("#E97916")}
+            >
+              Orange
+            </button>
+            <button className={styles.btn} onClick={() => changeColor("#c0c0c0")}>
+              Light Gray
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={[styles.optRow, "row"].join(" ")}>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Background Color</h4>
+            <button
+              className={styles.btn}
+              onClick={() => changeBGColor("#FF00F4")}
+            >
+              Magenta
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBGColor("#0E98F1")}
+            >
+              Blue
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBGColor("#E9E216")}
+            >
+              Yellow
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBGColor("transparent")}
+            >
+              None
+            </button>
+          </div>
+        </div>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Border Width</h4>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderSize("2px")}
+            >
+              2px
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderSize("4px")}
+            >
+              4px
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderSize("6px")}
+            >
+              6px
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderSize("0px")}
+            >
+              None
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={[styles.optRow, "row"].join(" ")}>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Border Color</h4>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderColor("#0B00FF")}
+            >
+              Blue
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderColor("#0EF167")}
+            >
+              Green
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderColor("#E9161D")}
+            >
+              Red
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderColor("#c0c0c0")}
+            >
+              Light Gray
+            </button>
+          </div>
+        </div>
+        <div className={[styles.optCol, "col"].join(" ")}>
+          <div className={styles.optionBox}>
+            <h4>Border Radius</h4>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderRadius("25px")}
+            >
+              Rounded
+            </button>
+            <button
+              className={styles.btn}
+              onClick={() => changeBorderRadius("0px")}
+            >
+              Squared
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StylesDemoComponent;
